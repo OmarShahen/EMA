eba608b1e9ee40f258fb5bc4ed279d13c06ff848
Omar Reda <omarredaelsayedmohamed@gmail.com>
2021-02-25 02:32:22 -0800

    Before changing the whole contact us page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, jsonify, redirect
 
 app = Flask(__name__)
 
@@ -6,3 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 def landing_page():
     return render_template('الصفحة-المقصودة.html')
+
+
+@app.route('/forms-submission', methods=["POST"])
+def forms():
+    user_name = request.form.get('user_name')
+    user_email = request.form.get('user_email')
+    user_phone = request.form.get('user_phone')
+    user_message = request.form.get('user_message').strip()
+    return redirect(url_for('landing_page'))
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
